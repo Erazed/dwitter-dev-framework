87b4f74eaa76d6c713aea97f688b0b25760d36dc
Erazed <spam_man@hotmail.co.uk>
2018-01-03 23:32:27 +0000

    solar system golfed

--- dweets/solarsystem.js
+++ dweets/solarsystem.js
@@ -1,10 +1,5 @@
 function u(t) {
-w=c.width=2e3;h=5e2
-for(i=1e-3;i<10;i++){X=1e3+i*99*C(5*t/i);Y=h+i*99*S(5*t/i)
-R=i<1?50:i>5?20:3
-x.moveTo(X,Y)
-x.ellipse(X,Y,R,R,0,0,9)}
-x.fill()
+w=c.width=2e3;h=5e2;for(i=1e-3;i<9;){X=1e3+i*99*C(3*t/i);Y=h+i*99*S(3*t/i);x.moveTo(X,Y);x.arc(X,Y,i++<1?60:i>6?25-2*i:i<5?i:3,0,9)}x.fill()
 }
 
 //r=[50,2,4,5,4,23,18,12,11]
