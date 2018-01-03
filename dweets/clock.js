7faadf37ede605f0279339e839622adfa0cdd859
Erazed <spam_man@hotmail.co.uk>
2018-01-03 00:24:16 +0000

    added clock dweet

---
+++ dweets/clock.js
@@ -0,0 +1,11 @@
+//https://www.dwitter.net/d/5328
+function u(t) {
+  w=c.width=1920
+  h=540
+  d=new Date()
+  H=d.getHours()-6+d.getMinutes()/60
+  x.arc(w/2,h,h/2,H*0.2618,99)
+  x.lineTo(w/2,h)
+  x.lineWidth=9
+  x.stroke()
+}
