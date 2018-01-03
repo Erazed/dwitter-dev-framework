9a69bcc8bc5a0dfa2bc7ca24274df1d2ac79ee3f
Erazed <spam_man@hotmail.co.uk>
2018-01-03 22:55:37 +0000

    improved solar system

--- dweets/solarsystem.js
+++ dweets/solarsystem.js
@@ -1,8 +1,8 @@
 function u(t) {
 w=c.width=2e3;h=5e2
-for(i=0;i<9;i++){X=1e3+i*99*C(5*t/i)||1e3;Y=h+i*99*S(5*t/i)||h
+for(i=1e-3;i<10;i++){X=1e3+i*99*C(5*t/i);Y=h+i*99*S(5*t/i)
+R=i<1?50:i>5?20:3
 x.moveTo(X,Y)
-R=50*C(i**5.15)**2
 x.ellipse(X,Y,R,R,0,0,9)}
 x.fill()
 }
--- dweets/test.js
+++ dweets/test.js
@@ -1,5 +1,16 @@
-function u(t) {
-  c.width = 1920;
-  for (i = 0; i < 9; i++)
-    x.fillRect(400 + i * 100 + S(t) * 300, 400, 2, 23)
+r=[50,2,4,5,4,23,18,12,11]
+var x = 0;
+while(x<100) {
+  var fucked = false;
+  for(var i = 0; i < 9; i++) {
+    var R=50*Math.cos(i**x)**2;
+    if(Math.abs(R-r[i]) > 10) {
+      console.log(i);
+      fucked = true;
+    }
+  }
+  if(!fucked)
+    console.log(x);
+  x+=0.001;
 }
+console.log('unlucky m8');
