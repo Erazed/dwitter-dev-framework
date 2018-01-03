8fea919a3571161a06502739114538b5ba531674
Erazed <spam_man@hotmail.co.uk>
2018-01-03 22:18:14 +0000

    started solar system

---
+++ dweets/solarsystem.js
@@ -0,0 +1,10 @@
+function u(t) {
+w=c.width=2e3;h=5e2
+for(i=0;i<9;i++){X=1e3+i*99*C(5*t/i)||1e3;Y=h+i*99*S(5*t/i)||h
+x.moveTo(X,Y)
+R=50*C(i**5.15)**2
+x.ellipse(X,Y,R,R,0,0,9)}
+x.fill()
+}
+
+//r=[50,2,4,5,4,23,18,12,11]
